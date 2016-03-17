db84ed9d427f88efcb32fe57116f5bc42f2239f4
ShiyangZhou <1120513218@qq.com>
2016-03-17 21:51:50 +0800

    First Github

---
+++ ACleetCode/ACleetCode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ACleetCode
+//
+//  Created by shiyang on 16-3-17.
+//  Copyright (c) 2016年 shiyang. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Github Hello, World!\n";
+    
+    return 0;
+}
